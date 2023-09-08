9a0e98b45d34379688f4b4ca4c45f9019e37a870
Hamza405 <hamzash863@gmail.com>
2023-09-08 21:55:14 +0100

    init routes

--- index.js
+++ index.js
@@ -4,9 +4,21 @@ const dotenv = require( "dotenv" );
 const helmet = require( "helmet" );
 const morgan = require( "morgan" );
 
+const userRoute = require( './routes/users' );
+const authRoute = require( './routes/auth' );
+
 dotenv.config();
 
 const app = express();
+
+// Middleware
+app.use( express.json() );
+app.use( helmet() );
+app.use( morgan( "common" ) );
+
+app.use( "/api/users", userRoute );
+app.use( "/api/auth", authRoute );
+
 mongoose.connect( process.env.MONGO_URL ).then( () => console.log( 'Connected Mongo' ) ).
     catch( error => console.log( error ) );
 
